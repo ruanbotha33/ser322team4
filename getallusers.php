f0984a19f7fed583288a37bd785919f877229c0c
ruanbotha33 <ruanbotha33@gmail.com>
2017-02-26 19:25:59 -0700

    Added other html files to be worked on

--- getallusers.php
+++ getallusers.php
@@ -3,9 +3,23 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Untitled Document</title>
+
+<script type="text/javascript" language="javascript" src="//cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
 </head>
 
 <body>
+
+
+<script>
+
+$(document).ready(function(){
+    $('#myTable').DataTable();
+});
+
+</script>
+
 
 <?php
 $q = intval($_GET['q']);
@@ -17,24 +31,38 @@ if (!$con) {
 }
 
 mysqli_select_db($con,"id899930_volunteers");
-$sql="SELECT * FROM volunteers WHERE Name = '".$q."'";
+$sql="SELECT * FROM volunteers";
 $result = mysqli_query($con,$sql);
-echo "Connected successfully";
-echo "<table>
+//echo "Connected successfully";
+/*?>echo "
+<div id='filter' class='dataTables_filter'
+<label>
+Search: <input type='search' class placeholder aria-controls='search'>
+</label>
+<div>
+";<?php */
+
+echo "<table id='myTable' class='display nowrap dataTable dtr-inline' data-searching='true' data-role ='datatable'>
 <tr>
 <th>Name</th>
 <th>ID</th>
 <th>Age</th>
+<th>Email</th>
+<th>Phone Number</th>
 <th>Home City</th>
 <th>Home State</th>
+<th>Total Hours</th>
 </tr>";
 while($row = mysqli_fetch_array($result)) {
     echo "<tr>";
     echo "<td>" . $row['Name'] . "</td>";
     echo "<td>" . $row['ID'] . "</td>";
     echo "<td>" . $row['Age'] . "</td>";
-    echo "<td>" . $row['homeCity'] . "</td>";
-    echo "<td>" . $row['homeState'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td>" . $row['phoneNumber'] . "</td>";
+	echo "<td>" . $row['homeCity'] . "</td>";
+	echo "<td>" . $row['homeState'] . "</td>";
+	echo "<td>" . $row['totalHours'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
